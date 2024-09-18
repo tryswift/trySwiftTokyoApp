4952b920440017f0682bcfabdcd9a23a463a29cf
Akio Itaya <17640999+AkkeyLab@users.noreply.github.com>
2024-09-18 14:11:47 +0900

    :sparkles: Created a website with Ignite (#56)
    * :sparkles: Created a website with Ignite
    
    * :recycle: Removed the duplicate .swiftpm directory
    
    * :recycle: Removed the duplicate Package.resolve file

---
+++ Website/trySwiftTokyo/Sources/ConferenceWebsite.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Ignite
+
+@main
+struct ConferenceWebsite {
+  static func main() async {
+    let site = ConferenceSite2025()
+
+    do {
+      try await site.publish()
+    } catch {
+      print(error.localizedDescription)
+    }
+  }
+}
+
+struct ConferenceSite2025: Site {
+  var name = "try! Swift Tokyo"
+  var url = URL("https://tryswift.jp")
+
+  var homePage = Home()
+  var theme = MainTheme()
+}
