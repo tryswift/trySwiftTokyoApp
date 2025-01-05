c2f908ced8e27d0cfc068deb954de0444d9553f0
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2025-01-06 02:50:13 +0900

    ♻️ Refactor code

--- Website/Package.swift
+++ Website/Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "Website",
-    defaultLocalization: "en",
-    platforms: [.macOS(.v14)],
-    dependencies: [
-        .package(url: "https://github.com/twostraws/Ignite", branch: "main"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.2.0"),
-        .package(path: "../MyLibrary")
-    ],
-    targets: [
-        .executableTarget(
-            name: "Website",
-            dependencies: [
-                "Ignite",
-                .product(name: "Dependencies", package: "swift-dependencies"),
-                .product(name: "DataClient", package: "MyLibrary"),
-            ],
-            resources: [
-                .process("Resources")
-            ]
-        ),
-    ]
+  name: "Website",
+  defaultLocalization: "en",
+  platforms: [.macOS(.v14)],
+  dependencies: [
+    .package(url: "https://github.com/twostraws/Ignite", branch: "main"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.2.0"),
+    .package(path: "../MyLibrary")
+  ],
+  targets: [
+    .executableTarget(
+      name: "Website",
+      dependencies: [
+        "Ignite",
+        .product(name: "Dependencies", package: "swift-dependencies"),
+        .product(name: "DataClient", package: "MyLibrary"),
+      ],
+      resources: [
+        .process("Resources")
+      ]
+    ),
+  ]
 )
