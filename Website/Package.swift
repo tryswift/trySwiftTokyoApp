95c250a7fbe75c35fc0f53f2e1488629ec6a120a
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2025-03-09 19:01:42 +0900

    :recycle: Changed the way to specify the Ignite version

--- Website/Package.swift
+++ Website/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
   defaultLocalization: "en",
   platforms: [.macOS(.v14)],
   dependencies: [
-    .package(url: "https://github.com/twostraws/Ignite", branch: "main"),
+    .package(url: "https://github.com/twostraws/Ignite", revision: "3080060aca91fe431b31d4a9b2bba2a99d81b25a"),
     .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.2.0"),
     .package(path: "../MyLibrary")
   ],
